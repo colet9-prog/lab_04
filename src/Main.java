3ad6c67fa11a88069defb49d20916d7a1fb740d9
Thomas <colet9@gmail.com>
2025-09-21 23:54:28 -0400

    Init

---
+++ src/Main.java
@@ -0,0 +1,69 @@
+public class Main {
+    public static void main(String[] args) {
+    int intOperandA = 1;
+    int intOperandB = 4;
+    int intSum = 32;
+    int intProduct = 5;
+    int intDifference = 9;
+    int intQuotient = 12;
+    int intModulo = 19;
+
+    double doubleOperandA = 13;
+    double doubleOperandB = 2;
+    double doubleSum = 2;
+    double doubleProduct = 16;
+    double doubleDifference = 18;
+    double doubleQuotient = 4;
+    double doubleModulo = 21;
+
+    intSum = intOperandA + intOperandB;
+    intProduct = intOperandA * intOperandB;
+    intDifference = intOperandA - intOperandB;
+    intQuotient = intOperandA / intOperandB;
+    intModulo = intOperandA % intOperandB;
+
+    System.out.println("Integer Sum: " + intSum);
+    System.out.println("Integer Product: " + intProduct);
+    System.out.println("Integer Difference: " + intDifference);
+    System.out.println("Integer Quotient: " + intQuotient);
+    System.out.println("Integer Modulo: " + intModulo);
+
+    doubleSum = doubleOperandA + doubleOperandB;
+    doubleProduct = doubleOperandA * doubleOperandB;
+    doubleDifference = doubleOperandA - doubleOperandB;
+    doubleQuotient = doubleOperandA / doubleOperandB;
+    doubleModulo = doubleOperandA % doubleOperandB;
+
+    System.out.println("Double Sum: " + doubleSum);
+    System.out.println("Double Product: " + doubleProduct);
+    System.out.println("Double Difference: " + doubleDifference);
+    System.out.println("Double Quotient: " + doubleQuotient);
+    System.out.println("Double Modulo: " + doubleModulo);
+
+    double myLunchCost = 12.50;
+    int familyKidTotal = 3;
+    Boolean isRaining = true;
+    double gasPrice = 4.32;
+    int favNum = 37;
+    double shoeSize = 11.5;
+    int birthMonth = 7;
+    String fullName = "Thomas Cole";
+
+    System.out.println("My lunch cost" + myLunchCost);
+    System.out.println("Kids in my family: " + familyKidTotal);
+    System.out.println("Is it Raining: " + isRaining);
+    System.out.println("A gallon of gas is " + gasPrice);
+    System.out.println("My favorite number is " + favNum);
+    System.out.println("My shoe size is " + shoeSize);
+    System.out.println("My birth month is " + birthMonth);
+    System.out.println("my full name is " + fullName);
+
+
+
+
+
+
+
+
+    }
+}
